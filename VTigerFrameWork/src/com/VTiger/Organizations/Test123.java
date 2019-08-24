a6bbc1682f89f4129f26328c3396369cc3a2244f
HP <HP@WINCTRL-921G759>
2019-08-24 16:11:20 +0530

    Test123 created by ankita

---
+++ VTigerFrameWork/src/com/VTiger/Organizations/Test123.java
@@ -0,0 +1,12 @@
+package com.VTiger.Organizations;
+
+import org.testng.annotations.Test;
+
+public class Test123 {
+
+	@Test
+	public void create()
+	{
+		System.out.println("Test Running");
+	}
+}
