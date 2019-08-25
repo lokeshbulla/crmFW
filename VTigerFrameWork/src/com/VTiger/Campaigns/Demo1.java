acb713482f7ee7906afeeeb245550b34103f59a2
Pallavi <pallavi.tambe92@gmail.com>
2019-08-25 11:05:11 +0530

    Demo1 commited by pallavi

---
+++ VTigerFrameWork/src/com/VTiger/Campaigns/Demo1.java
@@ -0,0 +1,12 @@
+package com.VTiger.Campaigns;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	@Test
+	public void demotest()
+	{
+		System.out.println("demo test running");
+	}
+
+}
